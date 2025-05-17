e87a31daeb38d201677961ffc71e13ed4b267c67
viv2711 <vksingh27112001@gmail.com>
2025-05-17 18:31:34 +0530

    update socials

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -11,10 +11,8 @@ const siteMetadata = {
     socialBanner: '/social-banner.png', // add social banner in the public folder
     email: 'codebucks27@gmail.com', 
     github: 'https://github.com/viv2711',
-    twitter: 'https://twitter.com/code_bucks',
     facebook: 'https://facebook.com',
-    youtube: 'https://youtube.com/codebucks',
-    linkedin: 'https://www.linkedin.com/in/codebucks/',
+    linkedin: 'https://www.linkedin.com/in/vivek-singh-user1/',
     dribbble: 'https://www.dribbble.com',
     locale: 'en-US',
   }
